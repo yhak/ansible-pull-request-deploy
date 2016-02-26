c4c6e47d0069d455ab81b3ac0ec692a16184caf8
yhak <yorihak@gmail.com>
2016-02-26 14:10:47 +0100

    Locally tested the Ansible deploy, now see what Travis does

--- ansible/deploy-pr.sh
+++ ansible/deploy-pr.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 ANSIBLE_ROOT=$(dirname $0)
-ANSIBLE_INVENTORY=$ANSIBLE_ROOT/vars/test-hosts
+ANSIBLE_INVENTORY=$ANSIBLE_ROOT/inventories/test-hosts
 ANSIBLE_PLAYBOOK=$ANSIBLE_ROOT/playbook.yml
-COMMIT_HASH=$(git fetch upstream && git rev-parse --verify upstream/master)
+COMMIT_HASH=$(git rev-parse --verify HEAD)
 
-ansible-playbook -i localhost, playbook.yml $COMMIT_HASH -vvvv
-## ansible-playbook -vv -i $ANSIBLE_INVENTORY $ANSIBLE_PLAYBOOK --extra-vars="commit_hash=$COMMIT_HASH"
+ansible-playbook -i localhost, $ANSIBLE_PLAYBOOK --extra-vars="commit_hash=$COMMIT_HASH" -vvvv
